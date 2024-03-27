c204f8f45e589f61d61f27171ce3288744d82820
KMS322 <kms930322@naver.com>
2024-03-27 16:28:10 +0900

    portfolio mobile change

--- front/src/constants.js
+++ front/src/constants.js
@@ -1,2 +1,2 @@
-// export const API_URL = "http://localhost:3060";
-export const API_URL = "http://pondhouse.kr:3060";
+export const API_URL = "http://localhost:3060";
+// export const API_URL = "http://pondhouse.kr:3060";
