795f46bdc2bf8133b358223ad5fe32d62d4e4fb6
KMS322 <kms930322@naver.com>
2024-03-28 10:50:52 +0900

    modal unchecked change

--- front/src/components/contact/contactS1.js
+++ front/src/components/contact/contactS1.js
@@ -44,6 +44,13 @@ const ContactS1 = () => {
 
       return () => clearTimeout(timeoutId);
     }
+    if (modalOpen && modalMsg === "unChecked") {
+      const timeoutId = setTimeout(() => {
+        setModalOpen(false);
+      }, 1500);
+
+      return () => clearTimeout(timeoutId);
+    }
   }, [modalOpen, modalMsg]);
 
   useEffect(() => {
